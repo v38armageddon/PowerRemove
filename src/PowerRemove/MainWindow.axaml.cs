937c82540955d0db031466aba476a8ab052c5d99
v38armageddon <69727502+v38armageddon@users.noreply.github.com>
2023-02-25 12:17:21 +0100

    Change .NET MAUI => Avalonia

---
+++ src/PowerRemove/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace PowerRemove
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
